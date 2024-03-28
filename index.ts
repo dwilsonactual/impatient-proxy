0fa6507918994cbefa00a427c15acbdb343ff7eb
David Wilson <dwilsonactual@github.com>
2024-03-28 13:47:32 -0400

    more

--- index.ts
+++ index.ts
@@ -18,7 +18,7 @@ proxy.onError(function (ctx, err) {
 
 // http://localhost:2206/github/chat/threads/5351c71d-b168-4e80-bf1e-aac83e17839f/messages
 proxy.onRequest(function (ctx, callback) {
-    console.log('proxy.onRequest', ctx.clientToProxyRequest.url, ctx.clientToProxyRequest.headers);
+    console.log('proxy.onRequest', ctx.clientToProxyRequest.url);
     if (ctx.clientToProxyRequest.headers.host === 'localhost:2206'
         && ctx.clientToProxyRequest.method === 'POST'
         && ctx.clientToProxyRequest.url?.match(/\/github\/chat\/threads\/[a-f0-9-]+\/messages/)) {
